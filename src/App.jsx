f1a1666868f9cd9ae0d7fa8f32f1a835f653a3a4
saulmartinx <122527980+saulmartinx@users.noreply.github.com>
2025-08-10 16:08:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import PokumaaLanding from './components/PokumaaLanding.jsx';
-import Quiz from ./components/Quiz.jsx';
+import Quiz from './components/Quiz.jsx';
 import { CartProvider } from './contexts/CartContext.jsx';
 
 function App() {
   return (
     <CartProvider>
-    
+      <PokumaaLanding />
       <div className="py-12 px-4">
         <Quiz />
       </div>
@@ -14,3 +14,4 @@ function App() {
 }
 
 export default App;
+
