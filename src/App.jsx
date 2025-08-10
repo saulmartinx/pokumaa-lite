2104735700615e2c40ee445244bf845e617b7750
saulmartinx <122527980+saulmartinx@users.noreply.github.com>
2025-08-10 05:54:24 -0700

    feat: wrap App with CartProvider and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import PokumaaLanding from './components/PokumaaLanding.jsx'
-import Quiz from './components/Quiz.jsx'
+
+import PokumaaLanding from './components/PokumaaLanding.jsx';
+import Quiz from './components/Quiz.jsx';
+import { CartProvider } from './contexts/CartContext.jsx';
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <PokumaaLanding />
       <div className="py-12 px-4">
         <Quiz />
       </div>
-    </>
-  )
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
