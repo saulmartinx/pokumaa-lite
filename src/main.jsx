5629df3b0dd797126d5ab74bd02e02318791686e
saulmartinx <122527980+saulmartinx@users.noreply.github.com>
2025-08-10 07:18:41 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+import { initGA4, initMetaPixel } from './lib/analytics.ts';
+
+// initialize analytics
+initGA4();
+initMetaPixel();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
